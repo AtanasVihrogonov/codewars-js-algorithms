27e558e902969282cae76ceeb7473ed659515927
Vihrogonov <vihrogonov@protonmail.com>
2022-06-21 23:13:49 +0100

    Revers words

---
+++ Revers words/index.js
@@ -0,0 +1,15 @@
+// DESCRIPTION:
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+// SOLUTION
+
+function reverseWords(str) {
+  //split words into separate arrays
+  return str.split('').reverse().join('').split(' ').reverse().join(' ')
+}
+
+console.log(reverseWords('test the function'))
